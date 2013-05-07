a4e380a22891e40f17a8cf3159db002cd671fbb6
Couto <luiscouto@triworks.net>
2013-05-07 12:23:00 +0100

    Fix errors to allow a stack

--- test/purgatory.test.js
+++ test/purgatory.test.js
@@ -4,8 +4,8 @@ var purgatory = require('../index.js'),
         var mocked = {
             req: { connection: { remoteAddress: ip } },
             resp: { statusCode: 200, end: function () {} },
-            next: function () { mocked.nextCalled = true; },
-            nextCalled: false
+            next: function (err) { mocked.error = err; },
+            error: null
         };
 
         return mocked;
@@ -14,62 +14,73 @@ var purgatory = require('../index.js'),
 module.exports = {
     'Bless' : {
         '127.0.0.1/32': function () {
-            var bless = purgatory(['127.0.0.1/32']).bless(),
+            var request = purgatory(['127.0.0.1/32']).bless(),
                 mocked = mock('127.0.0.1');
 
-            bless(mocked.req, mocked.resp, mocked.next);
+            request(mocked.req, mocked.resp, mocked.next);
 
-            assert.ok(mocked.nextCalled);
+            assert.doesNotThrow(function () {
+                assert.ifError(mocked.error);
+            }, Error);
             assert.equal(mocked.resp.statusCode, 200);
         },
         '192.30.252.0/22' : {
             '192.30.252.1': function () {
-                var bless = purgatory(['192.30.252.0/22']).bless(),
+                var request = purgatory(['192.30.252.0/22']).bless(),
                     mocked = mock('192.30.252.1');
 
-                bless(mocked.req, mocked.resp, mocked.next);
+                request(mocked.req, mocked.resp, mocked.next);
 
-                assert.ok(mocked.nextCalled);
+                assert.doesNotThrow(function () {
+                    assert.ifError(mocked.error);
+                }, Error);
                 assert.equal(mocked.resp.statusCode, 200);
             },
             '192.30.255.255': function () {
-                var bless = purgatory(['192.30.252.0/22']).bless(),
+                var request = purgatory(['192.30.252.0/22']).bless(),
                     mocked = mock('192.30.255.255');
 
-                bless(mocked.req, mocked.resp, mocked.next);
+                request(mocked.req, mocked.resp, mocked.next);
 
-                assert.ok(mocked.nextCalled);
+                assert.doesNotThrow(function () {
+                    assert.ifError(mocked.error);
+                }, Error);
                 assert.equal(mocked.resp.statusCode, 200);
             },
             '192.30.253.0': function () {
-                var bless = purgatory(['192.30.252.0/22']).bless(),
+                var request = purgatory(['192.30.252.0/22']).bless(),
                     mocked = mock('192.30.253.0');
 
-                bless(mocked.req, mocked.resp, mocked.next);
+                request(mocked.req, mocked.resp, mocked.next);
 
-                assert.ok(mocked.nextCalled);
+                assert.doesNotThrow(function () {
+                    assert.ifError(mocked.error);
+                }, Error);
                 assert.equal(mocked.resp.statusCode, 200);
             },
             '192.31.255.255': function () {
-                var bless = purgatory(['192.30.252.0/22']).bless(),
+                var request = purgatory(['192.30.252.0/22']).bless(),
                     mocked = mock('192.31.255.255');
 
-                bless(mocked.req, mocked.resp, mocked.next);
+                request(mocked.req, mocked.resp, mocked.next);
 
-                assert.ok(mocked.nextCalled === false);
-                assert.equal(mocked.resp.statusCode, 403);
+                assert.throws(function () {
+                    assert.ifError(mocked.error);
+                }, Error);
+                assert.equal(mocked.error.status, 403);
             }
         }
     },
     'Cursed' : {
         '127.0.0.1/32': function () {
-            var bless = purgatory(['127.0.0.1/32']).curse(),
+            var request = purgatory(['127.0.0.1/32']).curse(),
                 mocked = mock('127.0.0.1');
 
-            bless(mocked.req, mocked.resp, mocked.next);
-
-            assert.ok(mocked.nextCalled === false);
-            assert.equal(mocked.resp.statusCode, 403);
+            request(mocked.req, mocked.resp, mocked.next);
+            assert.throws(function () {
+                assert.ifError(mocked.error);
+            }, Error);
+            assert.equal(mocked.error.status, 403);
         }
     }
 
